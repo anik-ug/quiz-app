1a9a6bac7d1935b31bdff94c83117caa7a70d7a4
Anik Kumar <anikkumar.2712@gmail.com>
2026-04-08 01:05:11 +0530

    added CORS support

--- src/main/java/com/nick_ug/quizapp/QuizController.java
+++ src/main/java/com/nick_ug/quizapp/QuizController.java
@@ -14,6 +14,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @Tag(name =  "Quiz API",description = "Operations related to quiz management")
 @RestController
 @RequestMapping("/quiz")
